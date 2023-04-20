a183ee432c6fd2969eaa01f3b1ca4cb16e0e1a08
slemankassis <slemankassis@hotmail.com>
2023-04-20 20:39:14 -0300

    master: Fix tests CharacterDetail

--- src/components/CharacterDetail/CharacterDetail.test.tsx
+++ src/components/CharacterDetail/CharacterDetail.test.tsx
@@ -1,49 +1,94 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import { MockedProvider } from "@apollo/client/testing";
-import { MemoryRouter, Route } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import { useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
 import CharacterDetail from "./CharacterDetail";
-import { GET_CHARACTERS } from "../../graphql/queries";
 
-const mocks = [
-  {
-    request: {
-      query: GET_CHARACTERS,
-      variables: { id: 1 },
+jest.mock("react-router-dom", () => ({
+  useParams: jest.fn(),
+}));
+
+jest.mock("@apollo/client");
+
+describe("CharacterDetail", () => {
+  const mockCharacter = {
+    id: "1",
+    name: "Rick Sanchez",
+    status: "Alive",
+    species: "Human",
+    type: "",
+    gender: "Male",
+    origin: {
+      name: "Earth (C-137)",
+      type: "Planet",
+      dimension: "Dimension C-137",
     },
-    result: {
-      data: {
-        character: {
-          id: 1,
-          name: "Rick Sanchez",
-          status: "Alive",
-          species: "Human",
-          type: "",
-          gender: "Male",
-          image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-        },
-      },
+    location: {
+      name: "Earth (Replacement Dimension)",
+      type: "Planet",
+      dimension: "Replacement Dimension",
     },
-  },
-];
+    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  };
 
-test("renders character detail correctly", async () => {
-  const { findByText, findByAltText } = render(
-    <MockedProvider mocks={mocks} addTypename={false}>
-      <MemoryRouter initialEntries={["/character/1"]}>
-        <Route path="/character/:id">
-          <CharacterDetail />
-        </Route>
-      </MemoryRouter>
-    </MockedProvider>
-  );
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-  expect(await findByText("Loading...")).toBeInTheDocument();
+  test("renders loading message while data is loading", () => {
+    (useParams as jest.Mock).mockReturnValue({ id: "1" });
+    (useQuery as jest.Mock).mockReturnValue({ loading: true });
 
-  expect(await findByText("Name: Rick Sanchez")).toBeInTheDocument();
-  expect(await findByText("Status: Alive")).toBeInTheDocument();
-  expect(await findByText("Species: Human")).toBeInTheDocument();
-  expect(await findByText("Type:")).toBeInTheDocument();
-  expect(await findByText("Gender: Male")).toBeInTheDocument();
-  expect(await findByAltText("Rick Sanchez")).toBeInTheDocument();
+    render(<CharacterDetail />);
+
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
+  });
+
+  test("renders error message if there is an error", () => {
+    (useParams as jest.Mock).mockReturnValue({ id: "1" });
+    (useQuery as jest.Mock).mockReturnValue({ error: true });
+
+    render(<CharacterDetail />);
+
+    expect(screen.getByText("Error :(")).toBeInTheDocument();
+  });
+
+  test("renders character details when data is loaded", () => {
+    (useParams as jest.Mock).mockReturnValue({ id: "1" });
+    (useQuery as jest.Mock).mockReturnValue({
+      data: { character: mockCharacter },
+    });
+
+    render(<CharacterDetail />);
+
+    expect(screen.getByAltText(mockCharacter.name)).toBeInTheDocument();
+    expect(screen.getByText(mockCharacter.name)).toBeInTheDocument();
+    expect(
+      screen.getByText(`Status: ${mockCharacter.status}`)
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(`Species: ${mockCharacter.species}`)
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(`Gender: ${mockCharacter.gender}`)
+    ).toBeInTheDocument();
+
+    expect(screen.getByText("Origin")).toBeInTheDocument();
+    expect(
+      screen.getByText(`Name: ${mockCharacter.origin.name}`)
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByText(`Dimension: ${mockCharacter.origin.dimension}`)
+    ).toBeInTheDocument();
+
+    expect(screen.getByText("Location")).toBeInTheDocument();
+    expect(
+      screen.getByText(`Name: ${mockCharacter.location.name}`)
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByText(`Dimension: ${mockCharacter.location.dimension}`)
+    ).toBeInTheDocument();
+  });
 });
