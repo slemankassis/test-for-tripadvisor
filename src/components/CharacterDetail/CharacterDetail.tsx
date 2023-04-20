c145f2f279ede42dc1b61788781a0dfc8f14420a
slemankassis <slemankassis@hotmail.com>
2023-04-20 20:23:57 -0300

    master: Styling

--- src/components/CharacterDetail/CharacterDetail.tsx
+++ src/components/CharacterDetail/CharacterDetail.tsx
@@ -1,6 +1,7 @@
 import React, { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_CHARACTER } from "../../graphql/queries";
+import "./CharacterDetail.css"; // Import the CSS file
 
 function CharacterDetail() {
   const { id } = useParams<{ id: string }>();
@@ -14,25 +15,31 @@ function CharacterDetail() {
   const character = data.character;
 
   return (
-    <div>
-      <img src={character.image} alt={character.name} />
-      <h2>{character.name}</h2>
-      <p>Status: {character.status}</p>
-      <p>Species: {character.species}</p>
-      <p>Type: {character.type}</p>
-      <p>Gender: {character.gender}</p>
-      <h3>Origin</h3>
-      <ul>
-        <li>Name: {character.origin.name}</li>
-        <li>Type: {character.origin.type}</li>
-        <li>Dimension: {character.origin.dimension}</li>
-      </ul>
-      <h3>Location</h3>
-      <ul>
-        <li>Name: {character.location.name}</li>
-        <li>Type: {character.location.type}</li>
-        <li>Dimension: {character.location.dimension}</li>
-      </ul>
+    <div className="character-detail-container">
+      <img
+        src={character.image}
+        alt={character.name}
+        className="character-image"
+      />
+      <div className="character-info">
+        <h2>{character.name}</h2>
+        <p>Status: {character.status}</p>
+        <p>Species: {character.species}</p>
+        <p>Type: {character.type}</p>
+        <p>Gender: {character.gender}</p>
+        <h3>Origin</h3>
+        <ul>
+          <li>Name: {character.origin.name}</li>
+          <li>Type: {character.origin.type}</li>
+          <li>Dimension: {character.origin.dimension}</li>
+        </ul>
+        <h3>Location</h3>
+        <ul>
+          <li>Name: {character.location.name}</li>
+          <li>Type: {character.location.type}</li>
+          <li>Dimension: {character.location.dimension}</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/CharacterList/CharacterList.tsx
+++ src/components/CharacterList/CharacterList.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_CHARACTERS } from "../../graphql/queries";
 import { Character } from "../../graphql/types";
+import "./CharacterList.css"; // Import the CSS file
 
 function CharacterList() {
   const [page, setPage] = useState(1);
@@ -14,29 +15,33 @@ function CharacterList() {
   if (error) return <p>Error :(</p>;
 
   return (
-    <div>
+    <div className="character-list-container">
       {data.characters.results.map((character: Character) => (
-        <div key={character.id}>
+        <div key={character.id} className="character-card">
           <Link to={`/characters/${character.id}`}>
             <img src={character.image} alt={character.name} />
             <h2>{character.name}</h2>
           </Link>
         </div>
       ))}
-      <button
-        onClick={() => setPage(page - 1)}
-        disabled={!data.characters.info.prev}
-        data-testid="prev-page-button"
-      >
-        Previous Page
-      </button>
-      <button
-        onClick={() => setPage(page + 1)}
-        disabled={!data.characters.info.next}
-        data-testid="next-page-button"
-      >
-        Next Page
-      </button>
+      <div className="pagination-container">
+        <button
+          onClick={() => setPage(page - 1)}
+          disabled={!data.characters.info.prev}
+          data-testid="prev-page-button"
+          className="pagination-button"
+        >
+          Previous Page
+        </button>
+        <button
+          onClick={() => setPage(page + 1)}
+          disabled={!data.characters.info.next}
+          data-testid="next-page-button"
+          className="pagination-button"
+        >
+          Next Page
+        </button>
+      </div>
     </div>
   );
 }
